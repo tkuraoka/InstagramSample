bb530cb7444a0106823b70718d89f867ae4716c3
tkuraoka <takashisyocora@gmail.com>
2020-06-10 22:42:37 +0900

    Initial Commit

---
+++ InstaSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaSampleApp
+//
+//  Created by 倉岡隆志 on 2020/06/10.
+//  Copyright © 2020 TakashiKuraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
